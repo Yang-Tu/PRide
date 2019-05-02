70b0f16411a9cf325d6c4d457936293d380dc5e7
Yang T <yangt@princeton.edu>
2019-05-01 22:27:20 -0400

    updated team pics

--- Reunions_401/resources/views/business.blade.php
+++ Reunions_401/resources/views/business.blade.php
@@ -12,9 +12,14 @@
 	<br><br>
 		A wealth of knowledge and talent! <br>
 		<img src="alex.jpg" height = 500 width = 300>
+		<img src="alex3.PNG" height = 500 width = 300>
+		<img src="alex5.JPG" height = 500 width = 300>
 	<br><br>
 		Hire us for optimal solution! <br>
 		<img src = "yang.jpg" height = 300 width = 180>
+	<br><br>
+		Expertise at held in your hand<br>
+		<img src="DD.jpg" height = 300 width = 300>
 	<br>
 </body>
 
