b26cefa5ed1e90d2eb4e8a12ec63b1e1c714c368
Yang T <yangt@princeton.edu>
2019-05-08 21:42:18 -0400

    Our Approach Page
    text updated

--- Reunions_401/resources/views/method.blade.php
+++ Reunions_401/resources/views/method.blade.php
@@ -17,7 +17,7 @@
 	        </div>
 	        <div class="col-sm-6">
 	          <h2> Step 1: Sign Up </h2>
-	          First, sign up and place your preferences so we know which bucket to place you into for optimal matching. 
+	          First, sign up and select your prefered service so we know which bucket to place you into for optimal matching. For a description of different ways to ride with us, check out our services page. 
 	        </div>
 	      </div>
 
@@ -27,7 +27,7 @@
 	        </div>
 	       <div class="col-sm-6">
 	          <h2> Step 2: Matching Algorithm </h2>
-	          Now sit back, and let the matching algorithm do the work for you! Our strategy is much like an autonomous vehicle-- you're in the front seat, but you don't have to do any of the work!
+	          Now sit back, and let our matching algorithm do the work for you! We will pair you with a fellow rider that most closely matches your preferences and location.  Our strategy is much like a driverless vehicle--you're in the front seat, but you don't have to do any of the work.
 	        </div>
 	      </div>
 
@@ -37,7 +37,7 @@
 	        </div>
 	        <div class="col-sm-6">
 	          <h2> Step 3: You've got a match! </h2>
-	          Once the deadline to sign up for your event is over, we'll e-mail you notifying that you've got some matches. Then, you can log-in and view your new buds!
+	          Once the deadline to sign up for your event is over, we'll e-mail you notifying that you've got matches. Then, you can login and connect your new friends.
 	        </div>
 	      </div>
 
@@ -48,7 +48,7 @@
 	        </div>
 	        <div class="col-sm-6">
 	          <h2> Step 4: Ride in style. </h2>
-	           We hope you have fun at your event!
+	          You are all set to enjoy your trip with newfound convenience and the knowledge that you have made an impact in reducing your carbon footprint. We hope you have fun at your event!
 	        </div>
 	    </div>
 	</div>
